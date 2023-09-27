ab729f47519a4da844f3918d43bddb877cafbbd4
Vyshnavigudipudi <vyshnavigudipudi0@gmail.com>
2023-09-27 14:24:25 +0530

    integration of GIThub

---
+++ devopslab/src/devopslab/Demo.java
@@ -0,0 +1,11 @@
+package devopslab;
+
+public class Demo {
+	
+	public static void main(String args[]) {
+		//TODO Auto-generated method stub
+		System.out.println("Hi I am analyzing the code");
+		System.out.println("Hi I am analyzing the code");
+		System.out.println("Hi I am analyzing the code");
+	}
+}
